7180b28d2273cbf78b4338dea440a6733055ee7a
praveenprk <praveenkumarkp666@gmail.com>
2024-07-19 02:25:18 +0530

    use <Link> for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-[#0066dd] text-white py-4 mt-8">
       <div className="container mx-auto text-center">
-        <a href="mailto:oasis.repository@gmail.com" className="mx-4 hover:underline">
+        <Link href="mailto:oasis.repository@gmail.com" className="mx-4 hover:underline">
           Contact Us
-        </a>
-        <a href="https://www.oasis-of-ideas.com/legal/privacy-policy" className="mx-4 hover:underline">
+        </Link>
+        <Link href="https://www.oasis-of-ideas.com/legal/privacy-policy" className="mx-4 hover:underline">
           Privacy Policy
-        </a>
-        <a href="https://www.oasis-of-ideas.com/legal/terms-of-use" className="mx-4 hover:underline">
+        </Link>
+        <Link href="https://www.oasis-of-ideas.com/legal/terms-of-use" className="mx-4 hover:underline">
           Terms of Use
-        </a>
+        </Link>
       </div>
     </footer>
   );
